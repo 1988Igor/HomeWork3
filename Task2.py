16edede2a0c08b32e885500b4efa0b96a2e405ae
Igor <romanigor.conect@gmail.com>
2022-10-18 22:31:37 +0200

    add changes

--- Task2.py
+++ Task2.py
@@ -1,20 +1,14 @@
-#Напишите программу, которая найдёт c. 
+# Напишите программу, которая найдёт произведение пар чисел списка. 
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-
 # Пример:
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
-# lst = [2, 3, 4, 5, 6]
-# print(lst[0::3+1])
-# print(lst[0+1::3-1])
-
 def mult_lst(new_lst):
     temp_lst = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
     new_lst = [lst[i]*lst[len(lst)-i-1]for i in range(temp_lst)]
-    print (new_lst, end="")
-
-
+    return new_lst
+   
 lst = list(map(int, input("Enter the numbers through backspace:\n").split()))
-mult_lst(lst)
+print (f'The product of pairs of numbers in the list is : {mult_lst(lst)}')
